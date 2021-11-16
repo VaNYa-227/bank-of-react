35bd04b9bead0fee4c8a545c2b628d5ebb4fbf89
VaNYa-227 <44299437+VaNYa-227@users.noreply.github.com>
2021-11-16 01:45:30 -0500

    Added credits & debits +

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,8 +8,18 @@ class Home extends Component {
         <div>
           <img src="https://picsum.photos/201" alt="bank"/>
           <h1>Bank of React</h1>
-          
+
           <Link to="/userProfile">User Profile</Link>
+          <h1></h1>
+
+          <Link to="/LogIn">Log In</Link>
+          <h1></h1>
+
+          <Link to="/Credits">Credits</Link>
+          <h1></h1>
+
+          <Link to="/Debits">Debits</Link>
+          <h1></h1>
 
           <AccountBalance accountBalance={this.props.accountBalance}/>
         </div>
